64b90828208f1534e9405f0ca0152cc500b4907e
Krzysztof <k.olszewski@o2.pl>
2015-06-14 22:22:44 +0200

    formularz

--- src/patternbook/views.py
+++ src/patternbook/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response, HttpResponse
+from django.template import RequestContext
+from .forms import SignUpForm
 
-# Create your views here.
+
+def add_signup(request):
+    # Get the context from the request.
+    context = RequestContext(request)
+
+    # A HTTP POST?
+    if request.method == 'POST':
+        form = SignUpForm(request.POST or None)
+
+        # Have we been provided with a valid form?
+        if form.is_valid():
+            # Save the new category to the database.
+            form.save(commit=True)
+
+            # Now call the index() view.
+            # The user will be shown the homepage.
+            return HttpResponse('/about/')
+        else:
+            # The supplied form contained errors - just print them to the terminal.
+            print form.errors
+    else:
+        # If the request was not a POST, display the form to enter details.
+        form = SignUpForm()
+
+    # Bad form (or form details), no form supplied...
+    # Render the form with error messages (if any).
+    return render_to_response('templates/add_signup.html', {'form': form}, context)
