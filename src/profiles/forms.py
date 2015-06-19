43f9c26845fbdeaaa1ec4302b0aca253c51a2ff1
Krzysztof Olszewski <k.olszewski@o2.pl>
2015-06-19 14:24:17 +0200

    wzornik

--- src/profiles/forms.py
+++ src/profiles/forms.py
@@ -4,6 +4,7 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Div, Submit, HTML, Button, Row, Field
 from crispy_forms.bootstrap import AppendedText, PrependedText, FormActions
 from django.contrib.auth import get_user_model
+from django.utils.translation import ugettext as _
 from . import models
 
 User = get_user_model()
@@ -33,7 +34,7 @@ class ProfileForm(forms.ModelForm):
         self.helper.layout = Layout(
             Field('picture'),
             Field('bio'),
-            Submit('update', 'Update', css_class="btn-success"),
+            Submit('update', _("Update"), css_class="btn-success"),
             )
 
     class Meta:
